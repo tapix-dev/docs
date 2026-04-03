a05e938776b91fab8f3145711a2b9547809b1521
manukminasyan <manuk.minasyan1@gmail.com>
2026-04-03 11:34:23 +0400

    fix: remove target blank from header tapix.dev link

--- components/AppHeaderCTA.vue
+++ components/AppHeaderCTA.vue
@@ -1,7 +1,6 @@
 <template>
     <NuxtLink
         to="https://tapix.dev"
-        target="_blank"
         class="text-sm text-[var(--ui-text-muted)] hover:text-[var(--ui-text)] transition-colors"
     >
         tapix.dev
